08d242754575de4cc705c3b7ecb919a467f6c251
raduolaru1996 <raduolaru1996@gmail.com>
2022-05-21 15:35:49 +0300

    -l and -r implementation + error handling

--- LookAndSay/LookAndSay/LookAndSay.cs
+++ LookAndSay/LookAndSay/LookAndSay.cs
@@ -6,6 +6,70 @@ namespace LookAndSay
 {
     class LookAndSay
     {
+        int number;
 
+        public LookAndSay(int number)
+        {
+            this.number = number;
+        }
+
+        public int ConvertToLookAndSay()
+        {
+            string stringNr = number.ToString();
+            StringBuilder result = new StringBuilder();
+
+            char currentDigit = '0';
+            char previousDigit = '0';
+            int digitAmount = 0;
+            for (int i = 0; i < stringNr.Length; i++)
+            {
+                if(digitAmount == 0)
+                {
+                    currentDigit = stringNr[i];
+                    digitAmount++;
+                }
+                else
+                {
+                    if (currentDigit.Equals(stringNr[i]))
+                    {
+                        digitAmount++;
+                    }
+                    else
+                    {
+                        result.Append(digitAmount);
+                        result.Append(stringNr[i - 1]);
+
+                        digitAmount = 0;
+                        i--;
+                    }
+                }
+            }
+
+            result.Append(digitAmount);
+            result.Append(currentDigit);
+
+            return int.Parse(result.ToString());
+        }
+
+        public int ConvertToNumber()
+        {
+            if (number.ToString().Length % 2 != 0)
+            {
+                throw new Exception("To convert to the number it reflects it must have an even number of digits!");
+            }
+
+            string stringNr = number.ToString();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < stringNr.Length; i = i + 2)
+            {
+                int amount = int.Parse(stringNr[i].ToString());
+                for (int j = 0; j < amount; j++)
+                {
+                    result.Append(stringNr[i+1]);
+                }
+            }
+
+            return int.Parse(result.ToString());
+        }
     }
 }
--- LookAndSay/LookAndSay/Program.cs
+++ LookAndSay/LookAndSay/Program.cs
@@ -6,7 +6,56 @@ namespace LookAndSay
     {
         static void Main(string[] args)
         {
-            
+            int number;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please provide an argument [-l|-r] and a number");
+                return;
+            }
+            else
+            {
+                if (!(args[0].Equals("-l") || args[0].Equals("-r")))
+                {
+                    Console.WriteLine("Please provide an argument [-l|-r]");
+                    return;
+                }
+
+                if (!int.TryParse(args[1], out number))
+                {
+                    Console.WriteLine("Could not parse provided number");
+                    return;
+                }
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Please provide a positive number");
+                    return;
+                }
+            }
+
+            LookAndSay las = new LookAndSay(number);
+            int result;
+            try
+            {
+                switch (args[0])
+                {
+                    case "-l":
+                        result = las.ConvertToLookAndSay();
+                        break;
+                    case "-r":
+                        result = las.ConvertToNumber();
+                        break;
+                    default:
+                        throw new Exception("Unsuported operation");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"The following error occured: {e.Message}");
+                return;
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
